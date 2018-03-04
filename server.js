6d5e4be6156c96c191e7dcceee2b20e76e852db0
s-lavanya <31814245+s-lavanya@users.noreply.github.com>
2018-03-04 16:05:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 app.get('/article-one', function (req, res) {
   res.send('Article one requested and will be served here');
+});
 
 app.get('/article-two', function (req, res) {
   res.send('Article two requested and will be served here');

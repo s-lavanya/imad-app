6d2b51fab42a5e776b08b7dfdec1c63dd7006867
s-lavanya <31814245+s-lavanya@users.noreply.github.com>
2018-03-04 21:20:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,13 +119,6 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 
 // Do not change port, otherwise your app won't run on IMAD servers

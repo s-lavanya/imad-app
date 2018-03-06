28356cc81e6725aef748ab221fa0675ca3161aca
s-lavanya <31814245+s-lavanya@users.noreply.github.com>
2018-03-06 19:30:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById('name text');
 element.innerHTML = 'New value';
-var img = document.getElementById('madi');
+var img = document.getElementById('modi');
 img.onclick = function () {
     img.style.marginLeft = '100px';
     

9d93a73bb8df5741c6a3c4395f797573ee9eb3db
s-lavanya <31814245+s-lavanya@users.noreply.github.com>
2018-03-06 19:37:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var element = document.getElementById('name text');
 element.innerHTML = 'New value';
 var img = document.getElementById('madi');
 img.onclick = function () {
-    img.style.marginLeft = '100px';
+    
     var interval = setinterval(moveleft,100);
 };
